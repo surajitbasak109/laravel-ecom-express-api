8f65ecb70dfafd7fde38ca59f97cf5f64824f7a7
Surajit Basak <surajitbasak109@gmail.com>
2021-04-15 10:17:20 +0530

    minor changes

--- index.php
+++ index.php
@@ -6,10 +6,3 @@ require './vendor/autoload.php';
 
 use surajitbasak109\EcomExp\EcomExpApi;
 use surajitbasak109\EcomExp\Clients\EcomExpClient;
-
-$ecom = new EcomExpApi(new EcomExpClient());
-$token = $ecom->getToken(['username' => 'kidcitysolutionpvtltd19979_temp', 'password' => 'VHAZH7xFLp3VG9jr']);
-/* var_dump($token); */
-$response = $ecom->service($token)->checkServiceability();
-
-echo json_encode($response);
